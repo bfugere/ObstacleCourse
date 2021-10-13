aeee22ce18296bcb011437381a394c4978896f16
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-10-13 10:39:07 -0400

    Course Build & Score

--- Obstacle Course/Assets/Scripts/ScoreTally.cs
+++ Obstacle Course/Assets/Scripts/ScoreTally.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreTally : MonoBehaviour
 {
     int numberOfCollisions = 0;
+
+    Text bumpText;
+
+    void Start()
+    {
+        bumpText = GameObject.Find("Canvas/bumpText").GetComponent<Text>();
+        UpdateBumpText();
+    }
+
+    void Update()
+    {
+        UpdateBumpText();
+    }
 
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag != "Bumped")
         {
             numberOfCollisions++;
+            UpdateBumpText();
             Debug.Log(gameObject + " bumped into " + numberOfCollisions + " object(s).");
         }
+    }
+
+    void UpdateBumpText()
+    {
+        bumpText.text = "Bumps: " + numberOfCollisions.ToString();
     }
 }
